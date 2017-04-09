61bd95b7ff4871720589725cd95af01db3c6bb32
Gandalf1209 (Jonah) <criminal.jba@gmail.com>
2017-04-08 23:57:40 -0400

    Commit #0040 (Disassembler finished for now. Obviously will need to be constructed with everything else.)

--- src/Decomp.cpp
+++ src/Decomp.cpp
@@ -22,6 +22,13 @@ namespace VM {
 			std::printf("%04d: ", addr);
 			
 			switch(opcode) {
+				// NOP
+				case NOP: {
+					std::printf("NOP");
+					addr++;
+				}
+				
+				// MOV
 				case MOV_RN: {
 					std::printf("MOV \t%s %d", 
 					Token::getRegister(data[addr+1]), 
@@ -36,6 +43,24 @@ namespace VM {
 					addr += 3;
 					break;
 				}
+				
+				// ADD
+				case ADD_RN: {
+					std::printf("ADD \t%s %d", 
+					Token::getRegister(data[addr+1]), 
+					data[addr+2]);
+					addr += 3;
+					break;
+				}
+				case ADD_RR: {
+					std::printf("ADD \t%s %s", 
+					Token::getRegister(data[addr+1]), 
+					Token::getRegister(data[addr+2]));
+					addr += 3;
+					break;
+				}
+				
+				// SUB
 				case SUB_RN: {
 					std::printf("SUB \t%s %d", 
 					Token::getRegister(data[addr+1]), 
@@ -43,11 +68,37 @@ namespace VM {
 					addr += 3;
 					break;
 				}
+				case SUB_RR: {
+					std::printf("SUB \t%s %s", 
+					Token::getRegister(data[addr+1]), 
+					Token::getRegister(data[addr+2]));
+					addr += 3;
+					break;
+				}
+				
+				// CALL
+				case CALL_: {
+					std::printf("CALL \t0x%02d%02d",
+					data[addr+1], data[addr+2]);
+					addr += 3;
+					break;
+				}
+				
+				// RET
+				case RET_: {
+					std::printf("RET");
+					addr++;
+					break;
+				}
+				
+				// SYSI
 				case SYSI_: {
 					std::printf("SYSI");
 					addr++;
 					break;
 				}
+				
+				// MUL
 				case MUL_RN: {
 					std::printf("MUL \t%s %d", 
 					Token::getRegister(data[addr+1]), 
@@ -55,12 +106,90 @@ namespace VM {
 					addr += 3;
 					break;
 				}
+				case MUL_RR: {
+					std::printf("MUL \t%s %s", 
+					Token::getRegister(data[addr+1]), 
+					Token::getRegister(data[addr+2]));
+					addr += 3;
+					break;
+				}
+				
+				// MUL
+				case DIV_RN: {
+					std::printf("DIV \t%s %d", 
+					Token::getRegister(data[addr+1]), 
+					data[addr+2]);
+					addr += 3;
+					break;
+				}
+				case DIV_RR: {
+					std::printf("DIV \t%s %s", 
+					Token::getRegister(data[addr+1]), 
+					Token::getRegister(data[addr+2]));
+					addr += 3;
+					break;
+				}
+				
+				// SXR
+				case SXR_R: {
+					std::printf("SXR \t%s", 
+					Token::getRegister(data[addr+1]));
+					addr += 2;
+					break;
+				}
+				
+				// SXL
+				case SXL_R: {
+					std::printf("SXL \t%s", 
+					Token::getRegister(data[addr+1]));
+					addr += 2;
+					break;
+				}
+				
+				// INC
 				case INC_R: {
 					std::printf("INC \t%s", 
 					Token::getRegister(data[addr+1]));
 					addr += 2;
 					break;
 				}
+				
+				// DEC
+				case DEC_R: {
+					std::printf("DEC \t%s", 
+					Token::getRegister(data[addr+1]));
+					addr += 2;
+					break;
+				}
+				
+				// PUSH
+				case PUSH_R: {
+					std::printf("PUSH \t%s", 
+					Token::getRegister(data[addr+1]));
+					addr += 2;
+					break;
+				}
+				case PUSH_N: {
+					std::printf("PUSH \t%d", 
+					data[addr+1]);
+					addr += 2;
+					break;
+				}
+				
+				// POP
+				case POP_R: {
+					std::printf("POP \t%s", 
+					Token::getRegister(data[addr+1]));
+					addr += 2;
+					break;
+				}
+				case POP_X: {
+					std::printf("POP");
+					addr++;
+					break;
+				}
+				
+				// CMP
 				case CMP_RR: {
 					std::printf("CMP \t%s %s", 
 					Token::getRegister(data[addr+1]), 
@@ -68,6 +197,15 @@ namespace VM {
 					addr += 3;
 					break;
 				}
+				case CMP_RN: {
+					std::printf("CMP \t%s %d", 
+					Token::getRegister(data[addr+1]), 
+					data[addr+2]);
+					addr += 3;
+					break;
+				}
+				
+				// JUMPS
 				case JMP_: {
 					std::printf("JMP \t0x%02d%02d",
 					data[addr+1], data[addr+2]);
@@ -80,11 +218,10 @@ namespace VM {
 					addr += 3;
 					break;
 				}
-				case HLT_: {
-					running = false;
-					break;
-				}
+				
+				// DEFAULT
 				default: {
+					std::printf("???");
 					addr++;
 				}
 			}
